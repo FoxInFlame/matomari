3f826ef1e8666328a4b2e61a42be1f65e60a7a87
FoxInFlame <burningfoxinflame@gmail.com>
2016-12-09 00:04:37 +0100

    Reduced 7 lines
    Replaced all the echos to a single json_encode.
    
    Also changed some outputs.

--- api/animeInfo.php
+++ api/animeInfo.php
@@ -17,15 +17,18 @@ require("../SimpleHtmlDOM.php");
 
 $parts = isset($_GET['id']) ? explode('/',$_GET['id']) : array();
 if(!is_numeric($parts[0])) {
-  echo "Specified anime id is not a number.";
-  exit(400);
+  echo json_encode(array(
+    "error" => "Specified anime id is not a number."
+  ));
+  die();
 }
 $html = file_get_html("https://myanimelist.net/anime/" . $parts[0]);
 if(!$html) {
-  echo "Anime with specified id was not found.";
-  exit(404);
+  echo json_encode(array(
+    "error" => "Anime with specified id was not found."
+  ));
+  die();
 }
-
 
 //    [+] ============================================== [+]
 //    [+] --------------SETTING THE VALUES-------------- [+]
@@ -34,16 +37,16 @@ if(!$html) {
 $id = $parts[0];
 $title = substr($html->find("div#contentWrapper div h1.h1 span", 0)->plaintext, 0, -1);
 $alternativeTitles = $html->find("div#contentWrapper div#content table div.js-scrollfix-bottom .spaceit_pad");
-$alternativeTitles_eng = "null";
-$alternativeTitles_jap = "null";
-$alternativeTitles_syn = "null";
+$alternativeTitles_eng = null;
+$alternativeTitles_jap = null;
+$alternativeTitles_syn = null;
 foreach($alternativeTitles as $value) {
   if(strpos($value->plaintext, "English:") !== false) {
-    $alternativeTitles_eng = "\"".trim(substr($value->plaintext, 15), " ")."\"";
+    $alternativeTitles_eng = trim(substr($value->plaintext, 15), " ");
   } else if(strpos($value->plaintext, "Japanese:") !== false) {
-    $alternativeTitles_jap = "\"".trim(substr($value->plaintext, 16), " ")."\"";
+    $alternativeTitles_jap = trim(substr($value->plaintext, 16), " ");
   } else if(strpos($value->plaintext, "Synonyms:") !== false) {
-    $alternativeTitles_syn = "\"".trim(substr($value->plaintext, 16), " ")."\"";
+    $alternativeTitles_syn = trim(substr($value->plaintext, 16), " ");
   }
 }
 unset($value);
@@ -54,12 +57,16 @@ $mal_link = trim($html->find("div#contentWrapper div#content table div.js-scroll
 $information = $html->find("div#contentWrapper div#content div.js-scrollfix-bottom div");
 foreach($information as $value) {
   if(strpos($value->plaintext, "Type:") !== false) {
-    $type = trim(substr($value->plaintext, 9), " ");
+    $type = str_replace("</a>", "", trim(substr($value->plaintext, 9), " "));
+    // MAL has an ending tag without a starting tag bug so remove that
+    if($type == "Unknown") {
+      $type = null;
+    }
   }
   if(strpos($value->plaintext, "Episodes:") !== false) {
     $episodes = trim(substr($value->plaintext, 13), " ");
     if($episodes == "Unknown") {
-      $episodes = "null";
+      $episodes = null;
     }
   }
   if(strpos($value->plaintext, "Duration:") !== false) {
@@ -79,14 +86,24 @@ foreach($information as $value) {
   if(strpos($value->plaintext, "Score:") !== false) {
     if(strpos($value->plaintext, "users") !== false) {
       preg_match("/\d(,?\d?)+(?=  users)/", $value->plaintext, $matches);
-      $members_count = str_replace(",", "", $matches[0]);
+      $score_count = str_replace(",", "", $matches[0]);
     } else {
-      $members_count = "null";
+      $score_count = null;
     }
+  }
+  if(strpos($value->plaintext, "Members:") !== false) {
+    $members_count = trim(substr($value->plaintext, 12), " ");
+  }
+  if(strpos($value->plaintext, "Favorites:") !== false) {
+    $favourites_count = trim(substr($value->plaintext, 14), " ");
   }
   if(strpos($value->plaintext, "Genres:") !== false) {
     $genres_str = trim(substr($value->plaintext, 11), " ");
     $genres_arr = explode(", ", $genres_str);
+    $genres_arr = array_map("trim", $genres_arr);
+    if($genres_arr[0] == "No genres have been added yet.") {
+      $genres_arr = [];
+    }
   }
   if(strpos($value->plaintext, "Source:") !== false) {
     $source = trim(substr($value->plaintext, 11), " ");
@@ -94,6 +111,7 @@ foreach($information as $value) {
   if(strpos($value->plaintext, "Producers:") !== false) {
     $producers_str = trim(substr($value->plaintext, 16), " ");
     $producers_arr = explode(", ", $producers_str);
+    $producers_arr = array_map("trim", $producers_arr);
     if($producers_arr[0] == "None found") {
       $producers_arr = [];
     }
@@ -101,6 +119,7 @@ foreach($information as $value) {
   if(strpos($value->plaintext, "Studios:") !== false) {
     $studios_str = trim(substr($value->plaintext, 14), " ");
     $studios_arr = explode(", ", $studios_str);
+    $studios_arr = array_map("trim", $studios_arr);
     if($studios_arr[0] == "None found") {
       $studios_arr = [];
     }
@@ -108,6 +127,7 @@ foreach($information as $value) {
   if(strpos($value->plaintext, "Licensors:") !== false) {
     $licensors_str = trim(substr($value->plaintext, 16), " ");
     $licensors_arr = explode(", ", $licensors_str);
+    $licensors_arr = array_map("trim", $licensors_arr);
     if($licensors_arr[0] == "None found") {
       $licensors_arr = [];
     }
@@ -121,31 +141,31 @@ unset($value);
 //    [+] ============================================== [+]
 
 if(!isset($type)) {
-  $type = "null";
+  $type = null;
 }
 if(!isset($episodes)) {
-  $episodes = "null";
+  $episodes = null;
 }
 if(!isset($minutes)) {
-  $minutes = "null";
+  $minutes = null;
 }
-if(!isset($members_count)) {
-  $members_count = "null";
+if(!isset($score_count)) {
+  $score_count = null;
 }
 if(!isset($genres_arr)) {
   $genres_arr = [];
 }
-if($episodes != "null") {
+if($episodes != null) {
   $total_duration = intval($minutes) * intval($episodes);
 } else {
-  $total_duration = "null";
+  $total_duration = null;
 }
-$members_score = trim($html->find("div#contentWrapper div#content div.anime-detail-header-stats .score", 0)->plaintext, " ");
+$score = trim($html->find("div#contentWrapper div#content div.anime-detail-header-stats .score", 0)->plaintext, " ");
 if($rank == "/A") {
-  $rank = "null";
+  $rank = null;
 }
-if($members_score == "N/A") {
-  $members_score = "null";
+if($score == "N/A") {
+  $score = null;
 }
 $synopsis = trim($html->find("div#contentWrapper div#content div.js-scrollfix-bottom-rel table td span[itemprop=description]", 0)->innertext, " ");
 
@@ -158,61 +178,34 @@ $synopsis = trim($html->find("div#contentWrapper div#content div.js-scrollfix-bo
 // [+] ---------------------------------------------- [+]
 // [+] ============================================== [+]
 
-echo "{";
-  echo "\"id\": " . $id . ",";
-  echo "\"title\": \"" . $title . "\",";
-  echo "\"other_titles\": {";
-    echo "\"english\": ".$alternativeTitles_eng.",";
-    echo "\"japanese\": ".$alternativeTitles_jap.",";
-    echo "\"synonyms\": ".$alternativeTitles_syn;
-  echo "},";
-  echo "\"rank\": " . $rank . ",";
-  echo "\"popularity\": " . $popularity_rank . ",";
-  echo "\"image_url\": \"" . $image_url . "\",";
-  echo "\"source\": \"".$source."\",";
-  echo "\"url\": \"".$mal_link."\",";
-  echo "\"type\": \"" . $type . "\",";
-  echo "\"episodes\": " . $episodes . ",";
-  echo "\"duration\": " . $minutes . ",";
-  echo "\"total_duration\": " . $total_duration . ",";
-  echo "\"members_score\": " . $members_score . ",";
-  echo "\"members_count\": " . $members_count. ",";
-  echo "\"genres\": [";
-    foreach($genres_arr as $key => $value) {
-      if((count($genres_arr) - 1) == $key) {
-        echo "\"".$value."\"";
-      } else {
-        echo "\"".$value."\",";
-      }
-    }
-  echo "],";
-  echo "\"producers\": [";
-    foreach($producers_arr as $key => $value) {
-      if((count($producers_arr) - 1) == $key) {
-        echo "\"".trim($value, " ")."\"";
-      } else {
-        echo "\"".trim($value, " ")."\",";
-      }
-    }
-  echo "],";
-  echo "\"studios\": [";
-    foreach($studios_arr as $key => $value) {
-      if((count($studios_arr) - 1) == $key) {
-        echo "\"".trim($value, " ")."\"";
-      } else {
-        echo "\"".trim($value, " ")."\",";
-      }
-    }
-  echo "],";
-  echo "\"licensors\": [";
-    foreach($licensors_arr as $key => $value) {
-      if((count($licensors_arr) - 1) == $key) {
-        echo "\"".trim($value, " ")."\"";
-      } else {
-        echo "\"".trim($value, " ")."\",";
-      }
-    }
-  echo "],";
-  echo "\"synopsis\": \"".$synopsis ."\"";
-echo "}";
+$output = array(
+  "id" => $id,
+  "title" => $title,
+  "other_titles" => array(
+    "english" => $alternativeTitles_eng,
+    "japanese" => $alternativeTitles_jap,
+    "synonyms" => $alternativeTitles_syn
+  ),
+  "rank" => $rank,
+  "popularity" => $popularity_rank,
+  "image_url" => $image_url,
+  "source" => $source,
+  "url" => $mal_link,
+  "type" => $type,
+  "episodes" => $episodes,
+  "duration" => $minutes,
+  "total_duration" => $total_duration,
+  "score" => $score,
+  "score_count" => $score_count,
+  "members_count" => $members_count,
+  "favourites_count" => $favourites_count,
+  "genres" => $genres_arr,
+  "producers" => $producers_arr,
+  "studios" => $studios_arr,
+  "licensors" => $licensors_arr,
+  "synopsis" => $synopsis
+);
+
+// JSON_NUMERIC_CHECK flag requires at least PHP 5.3.3
+echo json_encode($output, JSON_NUMERIC_CHECK);
 ?>
