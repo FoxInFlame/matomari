80e4872187a6decde7a633d0569de4b97a940fdb
FoxInFlame <burningfoxinflame@gmail.com>
2016-12-09 00:53:59 +0100

    Update forumTopic.php

--- api/forumTopic.php
+++ api/forumTopic.php
@@ -1,8 +1,4 @@
 <?php
-ini_set("display_errors", 1);
-ini_set("display_startup_errors", 1);
-error_reporting(E_ALL);
-
 header("access-control-allow-origin: *");
 header("Content-Type: application/json");
 require("../SimpleHtmlDOM.php");
