c675190e9239e86424cde50e8c4571ee65d5c752
FoxInFlame <burningfoxinflame@gmail.com>
2016-12-08 23:40:51 +0100

    Reduced 43 lines
    Replaced all the echos to a single json_encode.

--- api/userInfo.php
+++ api/userInfo.php
@@ -18,9 +18,10 @@ require("../SimpleHtmlDOM.php");
 $parts = isset($_GET['username']) ? explode('/',$_GET['username']) : array();
 $html = file_get_html("https://myanimelist.net/profile/" . $parts[0]);
 if(!$html) {
-  echo "{";
-    echo "\"error\": \"Username was not found or MAL is offline.\"";
-  echo "}";
+  echo json_encode(array(
+    "error" => "Username was not found or MAL is offline."
+  ));
+  die();
 }
 
 
@@ -35,10 +36,10 @@ $html_leftside = $html->find("div#contentWrapper div.container-left", 0);
 $image_url = $html_leftside->find("div.user-profile div.user-image img", 0)->src;
 $id = explode(".", end(explode("/", $image_url)))[0];
 $userstatus = $html_leftside->find("div.user-profile ul.user-status li.clearfix");
-$gender = "null";
-$birthday = "null";
-$location = "null";
-$join_date = "null";
+$gender = null;
+$birthday = null;
+$location = null;
+$join_date = null;
 foreach($userstatus as $value) {
   if(strpos($value->plaintext, "Gender") !== false) {
     $gender = trim(substr($value->plaintext, 7), " ");
@@ -83,11 +84,11 @@ foreach($alsoat as $value) {
 }
 unset($value);
 $rss = $html_leftside->find("div.user-profile div.user-profile-sns", 1)->find("a");
-$rss_recentanime = "null";
-$rss_recentanime_byepisode = "null";
-$rss_recentmanga = "null";
-$rss_recentmanga_byepisode = "null";
-$rss_blogfeed = "null";
+$rss_recentanime = null;
+$rss_recentanime_byepisode = null;
+$rss_recentmanga = null;
+$rss_recentmanga_byepisode = null;
+$rss_blogfeed = null;
 foreach($rss as $value) {
   if(strpos($value->plaintext, "Recent Anime by Episode") !== false) {
     $rss_recentanime_byepisode = htmlspecialchars_decode($value->href);
@@ -101,6 +102,7 @@ foreach($rss as $value) {
     $rss_blogfeed = htmlspecialchars_decode($value->href);
   }
 }
+unset($value);
 $about = htmlspecialchars_decode(str_replace("\"", "'", trim($html_rightside->find("div.user-profile-about div.profile-about-user table tr td div.word-break", 0)->innertext, " ")));
 
 $favourites_anime_arr = array();
@@ -113,42 +115,34 @@ foreach($favourites as $value) {
     $favourites_anime = $value->find("ul.anime", 0);
     if(!empty($favourites_anime)) {
       foreach($favourites_anime->find("li") as $value) {
-        array_push($favourites_anime_arr, trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
+        // Add string_ to make sure PHP doesn't parse this as an integer
+        array_push($favourites_anime_arr, "string_".trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
       }
     }
   } else if($value->find("h5", 0)->innertext == "Manga") {
     $favourites_manga = $value->find("ul.manga", 0);
     if(!empty($favourites_manga)) {
       foreach($favourites_manga->find("li") as $value) {
-        array_push($favourites_manga_arr, trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
+        array_push($favourites_manga_arr, "string_".trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
       }
     }
   } else if($value->find("h5", 0)->innertext == "Characters") {
     $favourites_characters = $value->find("ul.characters", 0);
     if(!empty($favourites_characters)) {
       foreach($favourites_characters->find("li") as $value) {
-        array_push($favourites_characters_arr, trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
+        array_push($favourites_characters_arr, "string_".trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
       }
     }
   } else if($value->find("h5", 0)->innertext == "People") {
     $favourites_people = $value->find("ul.people", 0);
     if(!empty($favourites_people)) {
       foreach($favourites_people->find("li") as $value) {
-        array_push($favourites_people_arr, trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
+        array_push($favourites_people_arr, "string_".trim(explode("/", $value->find("div", 1)->find("a", 0)->href)[4]));
       }
     }
   }
 }
-
-
-//    [+] ============================================== [+]
-//    [+] --------------SETTING THE EMPTY--------------- [+]
-//    [+] ============================================== [+]
-
-if(!isset($type)) {
-  $type = "null";
-}
-
+unset($value);
 
 
 // [+] ============================================== [+]
@@ -157,83 +151,46 @@ if(!isset($type)) {
 // [+] ---------------------------------------------- [+]
 // [+] ============================================== [+]
 
-echo "{";
-  echo "\"id\": " . $id . ",";
-  echo "\"username\": \"" . $username . "\",";
-  echo "\"url\": \"".$mal_link."\",";
-  echo "\"image_url\": \"" . $image_url . "\",";
-  echo "\"gender\": \"" . $gender . "\",";
-  echo "\"birthday\": \"" . $birthday . "\",";
-  echo "\"location\": \"" . $location . "\",";
-  echo "\"join_date\": \"" . $join_date . "\",";
-  echo "\"animelist\": \"" . $animelist . "\",";
-  echo "\"mangalist\": \"" . $mangalist . "\",";
-  echo "\"history\": {";
-    echo "\"history_all\": \"" . $history . "\",";
-    echo "\"history_anime\": \"" . $history_anime . "\",";
-    echo "\"history_manga\": \"" . $history_manga . "\"";
-  echo "},";
-  echo "\"general_stats\": {";
-    echo "\"forum_posts\": " . $generalStats_forumposts . ",";
-    echo "\"reviews\": " . $generalStats_reviews . ",";
-    echo "\"recommendations\": " . $generalStats_recommendations . ",";
-    echo "\"blog_posts\": " . $generalStats_blogposts . ",";
-    echo "\"clubs\": " . $generalStats_clubs;
-  echo "},";
-  echo "\"also_at\": [";
-    foreach($alsoat_arr as $key => $value) {
-      if((count($alsoat_arr) - 1) == $key) {
-        echo "\"" . $value . "\"";
-      } else {
-        echo "\"" . $value . "\",";
-      }
-    }
-  echo "],";
-  echo "\"rss\": {";
-    echo "\"recent_anime\": \"" . $rss_recentanime . "\",";
-    echo "\"recent_anime_byepisode\": \"" . $rss_recentanime_byepisode . "\",";
-    echo "\"recent_manga\": \"" . $rss_recentmanga . "\",";
-    echo "\"recent_manga_bychapter\": \"" . $rss_recentmanga_bychapter . "\",";
-    echo "\"blogfeed\": \"" . $rss_blogfeed . "\"";
-  echo "},";
-  echo "\"summary\": \"" . $about . "\",";
-  echo "\"favourites\": {";
-    echo "\"anime\": [";
-      foreach($favourites_anime_arr as $key => $value) {
-        if((count($favourites_anime_arr) - 1) == $key) {
-          echo "\"".$value."\"";
-        } else {
-          echo "\"".$value."\",";
-        }
-      }
-    echo "],";
-    echo "\"manga\": [";
-      foreach($favourites_manga_arr as $key => $value) {
-        if((count($favourites_manga_arr) - 1) == $key) {
-          echo "\"".trim($value, " ")."\"";
-        } else {
-          echo "\"".trim($value, " ")."\",";
-        }
-      }
-    echo "],";
-    echo "\"characters\": [";
-      foreach($favourites_characters_arr as $key => $value) {
-        if((count($favourites_characters_arr) - 1) == $key) {
-          echo "\"".trim($value, " ")."\"";
-        } else {
-          echo "\"".trim($value, " ")."\",";
-        }
-      }
-    echo "],";
-    echo "\"people\": [";
-      foreach($favourites_people_arr as $key => $value) {
-        if((count($favourites_people_arr) - 1) == $key) {
-          echo "\"".trim($value, " ")."\"";
-        } else {
-          echo "\"".trim($value, " ")."\",";
-        }
-      }
-    echo "]";
-  echo "}";
-echo "}";
+$output = array(
+  "id" => $id,
+  "username" => $username,
+  "url" => $mal_link,
+  "gender" => $gender,
+  "birthday" => $birthday,
+  "location" => $location,
+  "join_date" => $join_date,
+  "animelist" => $animelist,
+  "mangalist" => $mangalist,
+  "history" => array(
+    "history_all" => $history,
+    "history_anime" => $history_anime,
+    "history_manga" => $history_manga
+  ),
+  "general_stats" => array(
+    "forum_posts" => $generalStats_forumposts,
+    "reviews" => $generalStats_reviews,
+    "recommendations" => $generalStats_recommendations,
+    "blog_posts" => $generalStats_blogposts,
+    "clubs" => $generalStats_clubs
+  ),
+  "also_at" => $alsoat_arr,
+  "rss" => array(
+    "recent_anime" => $rss_recentanime,
+    "recent_anime_byepisodes" => $rss_recentanime_byepisode,
+    "recent_manga" => $rss_recentmanga,
+    "recent_manga_bychapter" => $rss_recentmanga_bychapter,
+    "blogfeed" => $rss_blogfeed
+  ),
+  "summary" => $about,
+  "favourites" => array(
+    "anime" => $favourites_anime_arr,
+    "manga" => $favourites_manga_arr,
+    "characters" => $favourites_characters_arr,
+    "people" => $favourites_people_arr
+  )
+);
+
+// Remove string_ after parse
+// JSON_NUMERIC_CHECK flag requires at least PHP 5.3.3
+echo str_replace("string_", "", json_encode($output, JSON_NUMERIC_CHECK));
 ?>
