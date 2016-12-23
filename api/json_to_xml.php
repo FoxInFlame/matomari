d6efa9f50295688f55535853a4f91e9b2e9d689d
FoxInFlame <burningfoxinflame@gmail.com>
2016-12-23 22:08:44 +0100

    RESTful! Amazing!

---
+++ api/json_to_xml.php
@@ -0,0 +1,39 @@
+<?php
+// Thank you,
+// http://www.sean-barton.co.uk/2009/03/turning-an-array-or-object-into-xml-using-php/
+// I couldn't do this on my own.
+
+function generate_xml_from_array($array, $node_name) {
+  $xml = '';
+
+  if (is_array($array) || is_object($array)) {
+    foreach ($array as $key => $value) {
+      if (is_numeric($key)) {
+        $key = $node_name;
+      }
+
+      $xml .= "<" . $key . ">" . "\n" . generate_xml_from_array($value, $node_name) . "</" . $key . ">" . "\n";
+    }
+  } else {
+    $xml = htmlspecialchars($array) . "\n";
+  }
+  return $xml;
+}
+
+function generate_valid_xml_from_array($array, $node_block='matomari', $node_name='item') {
+  $xml = "<?xml version=\"1.0\" encoding=\"UTF-8\" ?>" . "\n";
+
+  $xml .= "<" . $node_block . ">" . "\n";
+  $xml .= generate_xml_from_array($array, $node_name);
+  $xml .= "</" . $node_block . ">" . "\n";
+
+  return $xml;
+}
+
+function json_to_xml($json) {
+  $array = json_decode(utf8_encode($json));
+  $xml_data = generate_valid_xml_from_array($array, "matomari", "item");
+  
+  return $xml_data;
+}
+?>
---
+++ api/main.php
@@ -0,0 +1,105 @@
+<?php
+header("access-control-allow-origin: *");
+$request = $_GET["url"];
+$request_parts = explode("/", $request);
+
+require("json_to_xml.php");
+
+dieIfNotSet($request_parts[0]);
+
+switch($request_parts[0]) {
+  case "anime": // anime/
+    dieIfNotSet($request_parts[1]);
+    dieIfNotSet($request_parts[2]);
+    $_GET["id"] = $request_parts[2];
+    switch($request_parts[1]) {
+      case "info": // anime/info/
+        showOutput("anime/info.php", $_GET["type"]);
+        break;
+      case "episodes": // anime/episodes/
+        break;
+      default:
+        die("Invalid Request");
+        break;
+    }
+    break;
+  case "user": // user/
+    dieIfNotSet($request_parts[1]);
+    dieIfNotSet($request_parts[2]);
+    $_GET["username"] = $request_parts[2];
+    switch($request_parts[1]) {
+      case "info": // user/info/
+        showOutput("user/info.php", $_GET["type"]);
+        break;
+      case "notifications": // user/notifications/
+        showOutput("user/notifications.php", $_GET["type"]);
+        break;
+      case "history": // user/history/
+        showOutput("user/history.php", $_GET["type"]);
+        break;
+      default:
+        die("Invalid Request");
+        break;
+    }
+    break;
+  case "forum": // forum/
+    dieIfNotSet($request_parts[1]);
+    dieIfNotSet($request_parts[2]);
+    $_GET["topicid"] = $request_parts[2];
+    switch($request_parts[1]) {
+      case "topic": // forum/topic/
+        showOutput("forum/topic.php", $_GET["type"]);
+        break;
+      default:
+        die("Invalid Request.");
+        break;
+    }
+    break;
+  case "general": // general/
+    // Files in the general category should be accessed directory through the file and not the REST interface.
+    die("Invalid Request.");
+    break;
+  case "club": // club/
+    dieIfNotSet($request_parts[1]);
+    dieIfNotSet($request_parts[2]);
+    $_GET["clubid"] = $request_parts[2];
+    switch($request_parts[1]) {
+      case "info": // club/info/
+        showOutput("club/info.php", $_GET["type"]);
+        break;
+      default:
+        die("Invalid Request.");
+        break;
+    }
+    break;
+  default:
+    die("Invalid Request.");
+    break;
+}
+
+function dieIfNotSet($part) {
+  if(empty($part) || !isset($part)) {
+    die("Invalid Request");
+    break;
+  }
+}
+
+function showOutput($request_file, $request_filetype) {
+  ob_start();
+  include($request_file);
+  $response_json = ob_get_clean();
+  switch(trim(strtolower($request_filetype))) {
+    case "json":
+      header("Content-Type: application/json");
+      echo $response_json;
+      break;
+    case "xml":
+      header("Content-Type: application/xml");
+      echo json_to_xml($response_json);
+      break;
+    default:
+      die("Invalid File Type.");
+      break;
+  }
+}
+?>
