2fd01de42b4857dd39a11215d9ce9e5e2e6317ed
FoxInFlame <burningfoxinflame@gmail.com>
2016-12-24 11:32:22 +0100

    Wrap in function for scope

--- api/user/history.php
+++ api/user/history.php
@@ -4,7 +4,7 @@
 Shows the history for a MAL user for the past 3 weeks (27 days).
 
 Method: GET
-        /api/user/history/CLUBID.(json|xml)
+        /api/user/history/USERNAME.(json|xml)
 Authentication: None Required.
 Supported Filetypes: json, xml
 Parameters:
@@ -26,129 +26,132 @@ header("Content-Type: application/json");
 header("Cache-Control: no-cache, must-revalidate");
 require(dirname(__FILE__) . "/../SimpleHtmlDOM.php");
 
+call_user_func(function() {
 
-// [+] ============================================== [+]
-// [+] ---------------------------------------------- [+]
-// [+] --------------GETTING THE VALUES-------------- [+]
-// [+] ---------------------------------------------- [+]
-// [+] ============================================== [+]
-
-$parts = isset($_GET["username"]) ? explode("/",$_GET["username"]) : array();
-if(empty($parts)) {
-  echo json_encode(array(
-    "error" => "The username parameter is not defined."
-  ));
-  die();
-}
-$type = "anime";
-if(isset($_GET["type"])) {
-  switch(strtolower($_GET["type"])) {
-    case "anime":
-      $type = "anime";
-      break;
-    case "manga":
-      $type = "manga";
-      break;
-    default:
-      $type = "anime";
+  // [+] ============================================== [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] --------------GETTING THE VALUES-------------- [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] ============================================== [+]
+  
+  $parts = isset($_GET['username']) ? explode("/",$_GET['username']) : array();
+  if(empty($parts)) {
+    echo json_encode(array(
+      "error" => "The username parameter is not defined."
+    ));
+    die();
   }
-}
-$html = @file_get_html("https://myanimelist.net/history/" . $parts[0] . "/" . $type);
-if(!$html) {
-  echo json_encode(array(
-    "error" => "Username was not found or MAL is offline."
-  ));
-  die();
-}
-
-
-//    [+] ============================================== [+]
-//    [+] --------------SETTING THE VALUES-------------- [+]
-//    [+] ============================================== [+]
-
-$username = $parts[0];
-$mal_link = "https://myanimelist.net/profile/" . $username;
-$history_tr = $html->find("div#contentWrapper div#content table tbody tr");
-$list = array();
-foreach($history_tr as $tr) {
-  if(!$tr->find("td.borderClass")) {
-    continue;
-  } else {
-    if($type == "anime") {
-      array_push($list, array(
-        "title" => $tr->find("td.borderClass a", 0)->innertext,
-        "id" => substr($tr->find("td.borderClass a", 0)->href, 14),
-        "episode" => $tr->find("td.borderClass strong", 0)->innertext,
-        "time" => getAbsoluteTimeGMT($tr->find("td.borderClass", 1)->plaintext)->format("c")
-      ));
-    } else if($type == "manga") {
-      array_push($list, array(
-        "title" => $tr->find("td.borderClass a", 0)->innertext,
-        "id" => substr($tr->find("td.borderClass a", 0)->href, 14),
-        "chapter" => $tr->find("td.borderClass strong", 0)->innertext,
-        "time" => getAbsoluteTimeGMT($tr->find("td.borderClass", 1)->plaintext)->format("c")
-      ));
+  $type = "anime";
+  if(isset($_GET['type'])) {
+    switch(strtolower($_GET['type'])) {
+      case "anime":
+        $type = "anime";
+        break;
+      case "manga":
+        $type = "manga";
+        break;
+      default:
+        $type = "anime";
     }
-    continue;
   }
-}
-
-function getAbsoluteTimeGMT($string) {
-  $string = trim($string); // Super important! :)
-  if(strpos($string, "ago") !== false) {
-    /*Note: These are returning approximate values */
-    $date = new DateTime(null);
-    $date->setTimeZone(new DateTimeZone("Etc/GMT"));
-    if(strpos($string, "hour") !== false) {
-      if(strpos($string, "hours") !== false) {
-        $hours = substr($string, 0, -10);
-        $date->modify("-" . $hours . " hours");
-      } else {
-        $hour = substr($string, 0, -9);
-        $date->modify("-" . $hour . " hour");
-      }
-    }
-    if(strpos($string, "minute") !== false) {
-      if(strpos($string, "minutes") !== false) {
-        $minutes = substr($string, 0, -12);
-        $date->modify("-" . $minutes . " minutes");
-      } else {
-        $minute = substr($string, 0, -11);
-        $date->modify("-" . $minute . " minute");
-      }
-    }
-    if(strpos($string, "second") !== false) {
-      if(strpos($string, "seconds") !== false) {
-        $seconds = substr($string, 0, -12);
-        $date->modify("-" . $seconds . " seconds");
-      } else {
-        $second = substr($string, 0, -11);
-        $date->modify("-" . $second . " second");
-      }
-    }
-    return $date;
-  } else if(strpos($string, "Today") !== false) {
-    $date = date_create_from_format("g:i A", substr($string, 7), new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
-    return $date;
-  } else if(strpos($string, "Yesterday") !== false) {
-    $date = date_create_from_format("g:i A", substr($string, 11), new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
-    $date->modify("-1 day");
-    return $date;
-  } else {
-    // "M j, g:i A" is the date type MAL shows
-    $date = date_create_from_format("M j, g:i A", $string, new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
-    return $date;
+  $html = @file_get_html("https://myanimelist.net/history/" . $parts[0] . "/" . $type);
+  if(!$html) {
+    echo json_encode(array(
+      "error" => "Username was not found or MAL is offline."
+    ));
+    die();
   }
-}
+  
+  
+  //    [+] ============================================== [+]
+  //    [+] --------------SETTING THE VALUES-------------- [+]
+  //    [+] ============================================== [+]
+  
+  $username = $parts[0];
+  $mal_link = "https://myanimelist.net/profile/" . $username;
+  $history_tr = $html->find("div#contentWrapper div#content table tbody tr");
+  $list = array();
+  foreach($history_tr as $tr) {
+    if(!$tr->find("td.borderClass")) {
+      continue;
+    } else {
+      if($type == "anime") {
+        array_push($list, array(
+          "title" => $tr->find("td.borderClass a", 0)->innertext,
+          "id" => substr($tr->find("td.borderClass a", 0)->href, 14),
+          "episode" => $tr->find("td.borderClass strong", 0)->innertext,
+          "time" => getAbsoluteTimeGMT($tr->find("td.borderClass", 1)->plaintext)->format("c")
+        ));
+      } else if($type == "manga") {
+        array_push($list, array(
+          "title" => $tr->find("td.borderClass a", 0)->innertext,
+          "id" => substr($tr->find("td.borderClass a", 0)->href, 14),
+          "chapter" => $tr->find("td.borderClass strong", 0)->innertext,
+          "time" => getAbsoluteTimeGMT($tr->find("td.borderClass", 1)->plaintext)->format("c")
+        ));
+      }
+      continue;
+    }
+  }
+  
+  function getAbsoluteTimeGMT($string) {
+    $string = trim($string); // Super important! :)
+    if(strpos($string, "ago") !== false) {
+      /*Note: These are returning approximate values */
+      $date = new DateTime(null);
+      $date->setTimeZone(new DateTimeZone("Etc/GMT"));
+      if(strpos($string, "hour") !== false) {
+        if(strpos($string, "hours") !== false) {
+          $hours = substr($string, 0, -10);
+          $date->modify("-" . $hours . " hours");
+        } else {
+          $hour = substr($string, 0, -9);
+          $date->modify("-" . $hour . " hour");
+        }
+      }
+      if(strpos($string, "minute") !== false) {
+        if(strpos($string, "minutes") !== false) {
+          $minutes = substr($string, 0, -12);
+          $date->modify("-" . $minutes . " minutes");
+        } else {
+          $minute = substr($string, 0, -11);
+          $date->modify("-" . $minute . " minute");
+        }
+      }
+      if(strpos($string, "second") !== false) {
+        if(strpos($string, "seconds") !== false) {
+          $seconds = substr($string, 0, -12);
+          $date->modify("-" . $seconds . " seconds");
+        } else {
+          $second = substr($string, 0, -11);
+          $date->modify("-" . $second . " second");
+        }
+      }
+      return $date;
+    } else if(strpos($string, "Today") !== false) {
+      $date = date_create_from_format("g:i A", substr($string, 7), new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
+      return $date;
+    } else if(strpos($string, "Yesterday") !== false) {
+      $date = date_create_from_format("g:i A", substr($string, 11), new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
+      $date->modify("-1 day");
+      return $date;
+    } else {
+      // "M j, g:i A" is the date type MAL shows
+      $date = date_create_from_format("M j, g:i A", $string, new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
+      return $date;
+    }
+  }
 
 
-// [+] ============================================== [+]
-// [+] ---------------------------------------------- [+]
-// [+] --------------------OUTPUT-------------------- [+]
-// [+] ---------------------------------------------- [+]
-// [+] ============================================== [+]
-
-// Remove string_ after parse
-// JSON_NUMERIC_CHECK flag requires at least PHP 5.3.3
-echo str_replace("string_", "", json_encode($list, JSON_NUMERIC_CHECK));
+  // [+] ============================================== [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] --------------------OUTPUT-------------------- [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] ============================================== [+]
+  
+  // Remove string_ after parse
+  // JSON_NUMERIC_CHECK flag requires at least PHP 5.3.3
+  echo str_replace("string_", "", json_encode($list, JSON_NUMERIC_CHECK));
+  
+});
 ?>
--- api/user/notifications.php
+++ api/user/notifications.php
@@ -26,63 +26,66 @@ header("Content-Type: application/json");
 header("Cache-Control: no-cache, must-revalidate");
 require(dirname(__FILE__) . "/../SimpleHtmlDOM.php");
 
+call_user_func(function() {
 
-// [+] ============================================== [+]
-// [+] ---------------------------------------------- [+]
-// [+] --------------------LOGIN--------------------- [+]
-// [+] ---------------------------------------------- [+]
-// [+] ============================================== [+]
-
-require("authenticate_base.php");
-
-if(!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
-  header('WWW-Authenticate: Basic realm="myanimelist.net"');
-  header('HTTP/1.0 401 Unauthorized');
-  echo json_encode(array(
-    "error" => "Authorisation Required."
-  ));
-  die();
-} else {
-  $MALsession = getSession($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
-}
-
-
-// [+] ============================================== [+]
-// [+] ---------------------------------------------- [+]
-// [+] --------------GET NOTIFICATIONS--------------- [+]
-// [+] ---------------------------------------------- [+]
-// [+] ============================================== [+]
-
-$ch = curl_init();
-curl_setopt($ch, CURLOPT_URL, "https://myanimelist.net/notification/all/new");
-curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-curl_setopt($ch, CURLOPT_HEADER, true);
-curl_setopt($ch, CURLOPT_COOKIE, $MALsession["cookie_string"]);
-curl_setopt($ch, CURLOPT_POST, 1);
-curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('csrf_token' => $MALsession["csrf_token"])));
-$response = curl_exec($ch);
-
-list($header, $body) = explode("\r\n\r\n", $response, 2);
-
-$html = @str_get_html($body);
-$children = $html->find("#contentWrapper", 0)->last_child();
-
-$notificationJS = $children->innertext;
-$notificationJS_0 = substr($notificationJS, 27);
-$notificationJS_1 = explode("; window.MAL.notification.hideEmpty = false; window.MAL.notification.templates = ", $notificationJS_0);
-
-// JavaScript object window.MAL.notification
-$window_MAL_notification = $notificationJS_1[0];
-
-// JavaScript object window.MAL.notification.templates
-$window_MAL_notification_templates = $notificationJS_1[1];
+  // [+] ============================================== [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] --------------------LOGIN--------------------- [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] ============================================== [+]
+  
+  require(dirname(__FILE__) . "/../authenticate_base.php");
+  
+  if(!isset($_SERVER['PHP_AUTH_USER']) || empty($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW']) || empty($_SERVER['PHP_AUTH_PW'])) {
+    header("WWW-Authenticate: Basic realm=\"myanimelist.net\"");
+    header("HTTP/1.0 401 Unauthorized");
+    echo json_encode(array(
+      "error" => "Authorisation Required."
+    ));
+    die();
+  } else {
+    $MALsession = getSession($_SERVER['PHP_AUTH_USER'], $_SERVER['PHP_AUTH_PW']);
+  }
 
 
-// [+] ============================================== [+]
-// [+] ---------------------------------------------- [+]
-// [+] --------------------OUTPUT-------------------- [+]
-// [+] ---------------------------------------------- [+]
-// [+] ============================================== [+]
+  // [+] ============================================== [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] --------------GET NOTIFICATIONS--------------- [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] ============================================== [+]
 
-echo $window_MAL_notification;
+  $ch = curl_init();
+  curl_setopt($ch, CURLOPT_URL, "https://myanimelist.net/notification/all/new");
+  curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+  curl_setopt($ch, CURLOPT_HEADER, true);
+  curl_setopt($ch, CURLOPT_COOKIE, $MALsession['cookie_string']);
+  curl_setopt($ch, CURLOPT_POST, 1);
+  curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query(array('csrf_token' => $MALsession['csrf_token'])));
+  $response = curl_exec($ch);
+  
+  list($header, $body) = explode("\r\n\r\n", $response, 2);
+  
+  $html = @str_get_html($body);
+  $children = $html->find("#contentWrapper", 0)->last_child();
+  
+  $notificationJS = $children->innertext;
+  $notificationJS_0 = substr($notificationJS, 27);
+  $notificationJS_1 = explode("; window.MAL.notification.hideEmpty = false; window.MAL.notification.templates = ", $notificationJS_0);
+
+  // JavaScript object window.MAL.notification
+  $window_MAL_notification = $notificationJS_1[0];
+  
+  // JavaScript object window.MAL.notification.templates
+  $window_MAL_notification_templates = $notificationJS_1[1];
+  
+  
+  // [+] ============================================== [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] --------------------OUTPUT-------------------- [+]
+  // [+] ---------------------------------------------- [+]
+  // [+] ============================================== [+]
+  
+  echo $window_MAL_notification;
+  
+});
 ?>
