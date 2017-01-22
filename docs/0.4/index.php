dea7c8bc573bf2a1ecb4104152b40a1532097df9
FoxInFlame <burningfoxinflame@gmail.com>
2017-01-22 20:25:28 +0100

    Added new warning message

--- docs/0.4/index.php
+++ docs/0.4/index.php
@@ -191,11 +191,18 @@ if(isset($_GET['file'])) {
       </div>
     </div>
     <div class="main ui container">
+      <div class="ui info icon message">
+        <i class="info sign icon"></i>
+        <div class="content">
+          <div class="header">Time to get secure</div>
+          <p>matomari is dropping regular HTTP support for requests on Feburary the 23rd. Use HTTPS for everything, baby!</p>
+        </div>
+      </div>
       <div class="ui warning icon message">
         <i class="warning sign icon"></i>
         <div class="content">
-          <div class="header">Time to get secure!</div>
-          <p>matomari is dropping regular HTTP support for requests on Feburary the 23rd. Use HTTPS for everything, baby!</p>
+          <div class="header">Attention!</div>
+          <p>Since the redirects have not been setup yet, the methods will not work unless you call the exact file name as in the source code.</p>
         </div>
       </div>
       <?php
