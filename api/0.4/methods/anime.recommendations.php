2541afee3017c4703f519235f444ef8fe7577259
FoxInFlame <burningfoxinflame@gmail.com>
2017-01-26 23:15:45 +0100

    Consistency across methods

--- api/0.4/methods/anime.recommendations.php
+++ api/0.4/methods/anime.recommendations.php
@@ -1,7 +1,7 @@
 <?php
 /*
 
-Shows latest anime recommendations.
+Get latest anime recommendations.
 
 Method: GET
         /anime/recommendations
@@ -24,6 +24,7 @@ header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Cache-Control: no-cache, must-revalidate");
 require_once(dirname(__FILE__) . "/../SimpleHtmlDOM.php");
+require_once(dirname(__FILE__) . "/../absoluteGMT.php");
 
 call_user_func(function() {
   
@@ -94,19 +95,19 @@ call_user_func(function() {
     array_push($recommendations_arr, array(
       "from" => array(
         "id" => $from_id,
+        "title" => $from_title,
         "image_1x" => $from_picture_1x,
-        "image_2x" => $from_picture_2x,
-        "title" => $from_title
+        "image_2x" => $from_picture_2x
       ),
       "to" => array(
         "id" => $to_id,
+        "title" => $to_title,
         "image_1x" => $to_picture_1x,
-        "image_2x" => $to_picture_2x,
-        "title" => $to_title
+        "image_2x" => $to_picture_2x
       ),
       "reason" => $reason,
       "author" => $author,
-      "time" => getAbsoluteTimeGMT($time)->format("c")
+      "time" => getAbsoluteTimeGMT($time, "M j, Y")->format("c")
     ));
   }
   
@@ -116,7 +117,9 @@ call_user_func(function() {
   // [+] ---------------------------------------------- [+]
   // [+] ============================================== [+]
   
-  $output = $recommendations_arr;
+  $output = array(
+    "items" => $recommendations_arr
+  );
   
   // Remove string_ after parse
   // JSON_NUMERIC_CHECK flag requires at least PHP 5.3.3
@@ -124,52 +127,4 @@ call_user_func(function() {
   http_response_code(200);
   
 });
-
-function getAbsoluteTimeGMT($string) {
-  $string = trim($string); // Super important! :)
-  if(strpos($string, "ago") !== false) {
-    /*Note: These are returning approximate values */
-    $date = new DateTime(null);
-    $date->setTimeZone(new DateTimeZone("Etc/GMT"));
-    if(strpos($string, "hour") !== false) {
-      if(strpos($string, "hours") !== false) {
-        $hours = substr($string, 0, -10);
-        $date->modify("-" . $hours . " hours");
-      } else {
-        $hour = substr($string, 0, -9);
-        $date->modify("-" . $hour . " hour");
-      }
-    }
-    if(strpos($string, "minute") !== false) {
-      if(strpos($string, "minutes") !== false) {
-        $minutes = substr($string, 0, -12);
-        $date->modify("-" . $minutes . " minutes");
-      } else {
-        $minute = substr($string, 0, -11);
-        $date->modify("-" . $minute . " minute");
-      }
-    }
-    if(strpos($string, "second") !== false) {
-      if(strpos($string, "seconds") !== false) {
-        $seconds = substr($string, 0, -12);
-        $date->modify("-" . $seconds . " seconds");
-      } else {
-        $second = substr($string, 0, -11);
-        $date->modify("-" . $second . " second");
-      }
-    }
-    return $date;
-  } else if(strpos($string, "Today") !== false) {
-    $date = date_create_from_format("g:i A", substr($string, 7), new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
-    return $date;
-  } else if(strpos($string, "Yesterday") !== false) {
-    $date = date_create_from_format("g:i A", substr($string, 11), new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
-    $date->modify("-1 day");
-    return $date;
-  } else {
-    // "M j, g:i A" is the date type MAL shows
-    $date = date_create_from_format("M j, Y", $string, new DateTimeZone("Etc/GMT+8"))->setTimeZone(new DateTimeZone("Etc/GMT"));
-    return $date;
-  }
-}
 ?>
