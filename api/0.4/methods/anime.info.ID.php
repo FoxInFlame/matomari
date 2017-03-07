a0dd8c2800f3682b7fff67b929b5b004bfd66d84
FoxInFlame <burningfoxinflame@gmail.com>
2017-03-07 23:26:14 +0100

    First step to caching---

--- api/0.4/methods/anime.info.ID.php
+++ api/0.4/methods/anime.info.ID.php
@@ -20,11 +20,17 @@ A Part of the matomari API.
 // [+] ---------------------------------------------- [+]
 // [+] ============================================== [+]
 
+
+ini_set("display_errors", true);
+ini_set("display_startup_errors", true);
+error_reporting(E_ALL);
+
 header("Access-Control-Allow-Origin: *");
 header("Content-Type: application/json");
 header("Cache-Control: no-cache, must-revalidate");
 require_once(dirname(__FILE__) . "/../SimpleHtmlDOM.php");
 require_once(dirname(__FILE__) . "/../class/class.anime.php");
+require_once(dirname(__FILE__) . "/../class/class.cache.php");
 
 call_user_func(function() {
   
@@ -49,12 +55,42 @@ call_user_func(function() {
     http_response_code(400);
     return;
   }
-  $html = @file_get_html("https://myanimelist.net/anime/" . $parts[0]);
-  if(!$html) {
+  
+  $url = "https://myanimelist.net/anime/" . $parts[0];
+  $data = new Data();
+  
+  if($data->getCache($url)) {
+    $html = str_get_html($data->data);
+  } else {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    $response = curl_exec($ch);
+    if(!$response) {
+       echo json_encode(array(
+        "message" => "MAL is offline."
+      ));
+      http_response_code(404);
+      return;
+    }
+    if(curl_getinfo($ch, CURLINFO_HTTP_CODE) === 404) {
+      echo json_encode(array(
+        "message" => "Anime with specified id could not be found."
+      ));
+      http_response_code(404);
+      return;
+    }
+    curl_close($ch);
+    
+    $data->saveCache($url, $response);
+    $html = str_get_html($response);
+  }
+  
+  if(!is_object($html)) {
     echo json_encode(array(
-      "message" => "Anime with specified id was not found."
+      "message" => "The code for MAL is not valid HTML markup.",
     ));
-    http_response_code(404);
+    http_response_code(500);
     return;
   }
   
