4c04b6605a6135d163222b6c8e77d43e5a37a888
FoxInFlame <burningfoxinflame@gmail.com>
2017-03-15 19:33:36 +0100

    Added recommendations/:id/:id

--- api/0.4/methods/anime.recommendations.ID.ID.php
+++ api/0.4/methods/anime.recommendations.ID.ID.php
@@ -1,11 +1,11 @@
 <?php
 /*
 
-Shows recommendations from an anime.
+Shows recommendations between two anime.
 
-This method is cached for an hour. Set the nocache parameter to true to use a fresh version (slower).
+This method is cached for a day. Set the nocache parameter to true to use a fresh version (slower).
 Method: GET
-        /anime/recommendations/:id
+        /anime/recommendations/:id/:id
 Authentication: None Required.
 Parameters:
   - None.
@@ -33,14 +33,16 @@ require_once(dirname(__FILE__) . "/../class/class.anime.php");
 require_once(dirname(__FILE__) . "/../class/class.cache.php");
 
 call_user_func(function() {
-  
+
   // [+] ============================================== [+]
   // [+] ---------------------------------------------- [+]
   // [+] --------------READING THE REQUEST------------- [+]
   // [+] ---------------------------------------------- [+]
   // [+] ============================================== [+]
-  
+
   $id = isset($_GET['id']) ? $_GET['id'] : "";
+  $id2 = isset($_GET['id2']) ? $_GET['id2'] : "";
+
   if(empty($id)) {
     echo json_encode(array(
       "message" => "The id parameter is not defined."
@@ -48,7 +50,14 @@ call_user_func(function() {
     http_response_code(400);
     return;
   }
-  if(!is_numeric($id)) {
+  if(empty($id2)) {
+    echo json_encode(array(
+      "message" => "Two ids are not defined."
+    ));
+    http_response_code(400);
+    return;
+  }
+  if(!is_numeric($id) || !is_numeric($id2)) {
     echo json_encode(array(
       "message" => "Specified anime id is not a number."
     ));
@@ -56,10 +65,10 @@ call_user_func(function() {
     return;
   }
 
-  $url = "https://myanimelist.net/anime/" . $id . "/FoxInFlameIsAwesome/userrecs";
+  $url = "https://myanimelist.net/recommendations/anime/" . $id . "-" . $id2;
   $data = new Data(); // Initialise cache class
-  
-  if($data->getCache($url, 60)) {
+
+  if($data->getCache($url)) {
     $html = str_get_html($data->data);
   } else {
     $ch = curl_init();
@@ -75,9 +84,9 @@ call_user_func(function() {
     }
     if(curl_getinfo($ch, CURLINFO_HTTP_CODE) === 404) {
       echo json_encode(array(
-        "message" => "Anime with specified id could not be found."
+        "recommendations" => array()
       ));
-      http_response_code(404);
+      http_response_code(200);
       return;
     }
     curl_close($ch);
@@ -85,7 +94,7 @@ call_user_func(function() {
     $data->saveCache($url, $response);
     $html = str_get_html($response);
   }
-  
+
   if(!is_object($html)) {
     echo json_encode(array(
       "message" => "The code for MAL is not valid HTML markup."
@@ -99,56 +108,18 @@ call_user_func(function() {
   // [+] --------------GET/SET THE VALUES-------------- [+]
   // [+] ---------------------------------------------- [+]
   // [+] ============================================== [+]
-  
-  $str = $html->find("#content table tbody tr", 0);
-  $recommendations = $html->find("div");
-  
-  $recommendations_arr = array();
+
+  $recommendations = $html->find("#content .borderDark .borderClass");
+  $anime_arr = array();
   foreach($recommendations as $recommendation) {
-    if(strpos($recommendation->class, "borderClass") === false) continue;
-    $to = $recommendation->find("table td", 0);
-    if(!$to) continue;
-    if(!$to->find("a.hoverinfo_trigger", 0)) continue;
-    
-    $to_anime = new Anime();
-    
-    $to_anime->set("id", explode("/", $to->find(".picSurround a", 0)->href)[2]);
-    $to_anime->set("image", $to->find(".picSurround a img", 0)->{'data-srcset'});
-    $to_anime->set("title", $recommendation->find("table td a strong", 0)->innertext);
-    $to_anime->set("mal_url", $recommendation->find("table td a", 0)->href);
-    $reason = explode("\r\n", substr($recommendation->find("table td", 1)->children(2)->find("div", 0)->plaintext, 0, -6));
-    $reason = str_replace(" &nbspread more", "", htmlspecialchars_decode(html_entity_decode(join("<br>", $reason), 0, "UTF-8")));
-    $author = trim($recommendation->find("table td", 1)->children(2)->children(1)->find("a", 1)->innertext);
-    $other = count($recommendation->find("table td a strong")) !== 1 ? $recommendation->find("table td a strong", 1)->innertext : "0";
-    $other_arr = array();
-    $other_elem = $recommendation->find("[id^=simaid]", 0);
-    if($other_elem) {
-      foreach($other_elem->find(".borderClass") as $otherrec) {
-        array_push($other_arr, array(
-          "reason" => str_replace(" &nbspread more", "", htmlspecialchars_decode(html_entity_decode(join("<br>", explode("\r\n", substr($otherrec->find(".spaceit_pad", 0)->plaintext, 0, -6))), 0, "UTF-8"))),
-          "author" => $otherrec->find(".spaceit_pad", 1)->find("a", 1)->innertext
-        ));
-      }
-    }
-    array_push($recommendations_arr, array(
-      "to" => array(
-        "id" => $to_anime->get("id"),
-        "image" => array(
-          "full" => $to_anime->get("image")[0],
-          "min" => $to_anime->get("image")[1]
-        ),
-        "title" => $to_anime->get("title"),
-        "url" => $to_anime->get("mal_url")
-      ),
+    $reason = explode("<span style=\"display:none\"", $recommendation->find(".spaceit_pad", 0)->innertext)[0] . $recommendation->find(".spaceit_pad span", 0)->innertext;
+    $author = $recommendation->find(".spaceit_pad", 1)->find("a", 0)->innertext;
+    array_push($anime_arr, array(
       "reason" => $reason,
-      "author" => $author,
-      "other_reviews" => array(
-        "total" => $other,
-        "items" => $other_arr
-      )
+      "author" => $author
     ));
   }
-
+  
   // [+] ============================================== [+]
   // [+] ---------------------------------------------- [+]
   // [+] --------------------OUTPUT-------------------- [+]
@@ -162,7 +133,4 @@ call_user_func(function() {
   // JSON_NUMERIC_CHECK flag requires at least PHP 5.3.3
   echo json_encode($output, JSON_NUMERIC_CHECK);
   http_response_code(200);
-  
 });
-
-?>
