a2b62ef9e59b21c30f0e4511b32dd5608ba82adc
FoxInFlame <burningfoxinflame@gmail.com>
2017-03-27 23:12:44 +0200

    Initialise on document ready

--- js/docs.js
+++ js/docs.js
@@ -1,8 +1,8 @@
 $("#freewha").remove();
 $(".ui.dropdown").dropdown();
 
-$(".ui.sidebar").sidebar("show");
-
-if($(".property").length !== 0) {
-  $(".property").popup();
-}
+$(document).ready(function() {
+  if($(".property").length !== 0) {
+    $(".property").popup();
+  }
+});
