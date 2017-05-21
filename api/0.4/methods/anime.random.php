0182780345e9a8372bb1855b19bbe19f140d86ce
FoxInFlame <burningfoxinflame@gmail.com>
2017-05-21 19:07:14 +0200

    Add parameter to exclude ids

--- api/0.4/methods/anime.random.php
+++ api/0.4/methods/anime.random.php
@@ -1,14 +1,14 @@
 <?php
 /*
 
-Get a random anime. It will 302 redirect to /anime/info/:ID
+Get a random anime. It will 302 redirect to /anime/info/:id
 
 This method is not cached.
 Method: GET
         /anime/random
 Authentication: None Required.
 Parameters:
-  - None.
+  - not: [Optional] List of ids to not include in the random, separated by comma
 
 Created by FoxInFlame.
 A Part of the matomari API.
@@ -27,6 +27,9 @@ header("Cache-Control: no-cache, must-revalidate");
 
 call_user_func(function() {
   
+  isset($_GET['not']) && !empty($_GET['not']) ? $not = $_GET['not'] : $not = "";
+  $not_arr = explode(",", $not);
+  
   // [+] ============================================== [+]
   // [+] ---------------------------------------------- [+]
   // [+] ----------GETTING A RANDOM ANIME ID----------- [+]
@@ -37,8 +40,10 @@ call_user_func(function() {
   while (!$success_id) {
     $id = mt_rand(1, 35545); // Highest as of May 21, 2017
     
+    if(in_array($id, $not_arr)) continue; // Skip if it's in the not_arr
+    
     $ch = curl_init();
-    curl_setopt($ch, CURLOPT_URL, "https://myanimelist.net/anime/" . $id);
+    curl_setopt($ch, CURLOPT_URL, "https://myanimelist.net/anime/" . $id . "/FoxInFlameIsAwesome/news"); // /news always exists and is pretty light-weight
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
     
     $response = curl_exec($ch);
@@ -59,6 +64,6 @@ call_user_func(function() {
   // [+] ============================================== [+]
   
   http_response_code(302);
-  header('Location: /api/0.4/methods/anime.info.ID.php?id=' . $success_id, true, 302);
+  header('Location: https://www.matomari.tk/api/0.4/methods/anime.info.ID.php?id=' . $success_id, true, 302); // TODO: Change this URL to REST
 
 });
