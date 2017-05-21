6758c48a1f352bc4623996dd9d7b278f1dc42cd9
FoxInFlame <burningfoxinflame@gmail.com>
2017-05-21 20:06:16 +0200

    Transition on popup and hoverable

--- js/docs.js
+++ js/docs.js
@@ -3,6 +3,9 @@ $(".ui.dropdown").dropdown();
 
 $(document).ready(function() {
   if($(".tooltip, .property, span[class^='type-'").length !== 0) {
-    $(".tooltip, .property, span[class^='type-']").popup();
+    $(".tooltip, .property, span[class^='type-']").popup({
+      hoverable: true,
+      transition: "fade"
+    });
   }
 });
