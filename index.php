558fa09476836ce515e48fba565717f853ac9924
FoxInFlame <burningfoxinflame@gmail.com>
2017-10-25 22:00:29 +0900

    Add index.

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+  <head>
+    <title>Index</title>
+  </head>
+  <body>
+    matomari. The perfect way to access MyAnimeList through code.
+    <br>
+    <br>
+    <br>
+    <small>Developed by <a href="https://www.foxinflame.tk/">FoxInFlame</a></small>
+  </body>
+</html>
