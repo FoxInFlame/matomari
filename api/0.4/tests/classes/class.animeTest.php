c8ccdec20514a6a1e16245f8829107b1467396c7
FoxInFlame <burningfoxinflame@gmail.com>
2017-11-02 21:19:46 +0900

    Reread the documentation for PHPUnit to realise the arguments were in the wrong order

--- api/0.4/tests/classes/class.animeTest.php
+++ api/0.4/tests/classes/class.animeTest.php
@@ -9,49 +9,49 @@ class AnimeTest extends TestCase {
     $anime = new Anime();
     $animeID = rand(1, 50000);
     $anime->set("id", $animeID);
-    $this->assertEquals($anime->get("id"), $animeID);
+    $this->assertEquals($animeID, $anime->get("id"));
   }
 
   public function testTitle() {
     $anime = new Anime();
     $animeTitle = str_shuffle("FoxInFlame is an admirable person.");
     $anime->set("title", $animeTitle);
-    $this->assertEquals($anime->get("title"), $animeTitle);
+    $this->assertEquals($animeTitle, $anime->get("title"));
   }
 
   public function testMal_Url() {
     $anime = new Anime();
     $animeMal_Url = "https://myanimelist.net/anime/" . (string)rand(1, 50000);
     $anime->set("mal_url", $animeMal_Url);
-    $this->assertEquals($anime->get("mal_url"), $animeMal_Url);
+    $this->assertEquals($animeMal_Url, $anime->get("mal_url"));
   }
 
   public function testImage_Url() {
     $anime = new Anime();
     $animeImage_Url = "https://myanimelist.cdn-dena.com/images/anime/6/" . (string)rand(1, 50000) . "/.jpg";
     $anime->set("image_url", $animeImage_Url);
-    $this->assertEquals($anime->get("image_url"), $animeImage_Url);
+    $this->assertEquals($animeImage_Url, $anime->get("image_url"));
   }
 
   public function testScore() {
     $anime = new Anime();
     $animeScore = rand(pow(10, 2), pow(10, 3) - 1) / 100;
     $anime->set("score", $animeScore);
-    $this->assertEquals($anime->get("score"), $animeScore);
+    $this->assertEquals($animeScore, $anime->get("score"));
   }
 
   public function testRank() {
     $anime = new Anime();
     $animeRank = rand(1, 50000);
     $anime->set("rank", $animeRank);
-    $this->assertEquals($anime->get("rank"), $animeRank);
+    $this->assertEquals($animeRank, $anime->get("rank"));
   }
 
   public function testPopularity() {
     $anime = new Anime();
     $animePopularity = rand(1, 50000);
     $anime->set("popularity", $animePopularity);
-    $this->assertEquals($anime->get("popularity"), $animePopularity);
+    $this->assertEquals($animePopularity, $anime->get("popularity"));
   }
 
   public function testSynopsis() {
@@ -60,7 +60,7 @@ class AnimeTest extends TestCase {
 <br />
 Fast forward two and a half years and two of those members, Aoi Miyamori and Ema Yasuhara, have made their dreams into reality by landing jobs at a famous production company called Musashino Animation. Everything seems perfect at first. However, as the girls slowly discover, the animation industry is a bit tougher than they had imagined. Who said making your dream come true was easy?");
     $anime->set("synopsis", $animeSynopsis);
-    $this->assertEquals($anime->get("synopsis"), $animeSynopsis);
+    $this->assertEquals($animeSynopsis, $anime->get("synopsis"));
   }
 
   public function testOther_Titles() {
@@ -72,28 +72,28 @@ Fast forward two and a half years and two of those members, Aoi Miyamori and Ema
       "synonyms" => array('That', 'I', 'Don\'t', 'Care.')
     );
     $anime->set("other_titles", $animeOther_Titles);
-    $this->assertEquals($anime->get("other_titles"), $animeOther_Titles);
+    $this->assertEquals($animeOther_Titles, $anime->get("other_titles"));
   }
 
   public function testType() {
     $anime = new Anime();
     $animeType = array_rand(array('TV', 'Wow it\'s an OVA', 'Music', 'MOVIE!'));
     $anime->set("type", $animeType);
-    $this->assertEquals($anime->get("type"), $animeType);
+    $this->assertEquals($animeType, $anime->get("type"));
   }
 
   public function testEpisodes() {
     $anime = new Anime();
     $animeEpisodes = rand();
     $anime->set("episodes", $animeEpisodes);
-    $this->assertEquals($anime->get("episodes"), $animeEpisodes);
+    $this->assertEquals($animeEpisodes, $anime->get("episodes"));
   }
 
   public function tesetAir_Status() {
     $anime = new Anime();
     $animeAir_Status = str_shuffle('Currently Airing');
     $anime->set("air_status", $animeAir_Status);
-    $this->assertEquals($anime->get("animeAir_Status"), $animeAir_Status);
+    $this->assertEquals($animeAir_Status, $anime->get("animeAir_Status"));
   }
 
   public function testAir_Dates() {
@@ -102,64 +102,64 @@ Fast forward two and a half years and two of those members, Aoi Miyamori and Ema
     $animeAir_Date_To = str_shuffle('2015-03-26T08:00:00+00:00');
     $anime->set("air_date_from", $animeAir_Date_From);
     $anime->set("air_date_to", $animeAir_Date_To);
-    $this->assertEquals($anime->get("air_date_from"), $animeAir_Date_From);
-    $this->assertEquals($anime->get("air_date_to"), $animeAir_Date_To);
+    $this->assertEquals($animeAir_Date_From, $anime->get("air_date_from"));
+    $this->assertEquals($animeAir_Date_To, $anime->get("air_date_to"));
   }
 
   public function testPremier_Date() {
     $anime = new Anime();
     $animePremier_Date = str_shuffle('2014-10-09T08:00:00+00:00');
     $anime->set("premier_date", $animePremier_Date);
-    $this->assertEquals($anime->get("premier_date"), $animePremier_Date);
+    $this->assertEquals($animePremier_Date, $anime->get("premier_date"));
   }
 
   public function testSeason() {
     $anime = new Anime();
     $animeSeason = array_rand(array('Fall ', 'Summer ', 'Winter ', 'Fox ')) . (string)rand(1, 3000);
     $anime->set("season", $animeSeason);
-    $this->assertEquals($anime->get("season"), $animeSeason);
+    $this->assertEquals($animeSeason, $anime->get("season"));
   }
 
   public function testAir_Time() {
     $anime = new Anime();
     $animeAir_Time = str_shuffle("Thursdays at 23:30 (JST)");
     $anime->set("air_time", $animeAir_Time);
-    $this->assertEquals($anime->get("air_time"), $animeAir_Time);
+    $this->assertEquals($animeAir_Time, $anime->get("air_time"));
   }
 
   public function testProducers() {
     $anime = new Anime();
     $animeProducers = array("Sotsu", "Movic", "Warner Bros.", "KlockWorx", "Showgate", "Infinite");
     $anime->set("producers", $animeProducers);
-    $this->assertEquals($anime->get("producers"), $animeProducers);
+    $this->assertEquals($animeProducers, $anime->get("producers"));
   }
 
   public function testLicensors() {
     $anime = new Anime();
     $animeLicensors = array("Sentai Filmworks");
     $anime->set("licensors", $animeLicensors);
-    $this->assertEquals($anime->get("licensors"), $animeLicensors);
+    $this->assertEquals($animeLicensors, $anime->get("licensors"));
   }
 
   public function testStudios() {
     $anime = new Anime();
     $animeStudios = array("P.A. Works", "Kyoto Animation");
     $anime->set("studios", $animeStudios);
-    $this->assertEquals($anime->get("studios"), $animeStudios);
+    $this->assertEquals($animeStudios, $anime->get("studios"));
   }
 
   public function testSource() {
     $anime = new Anime();
     $animeSource = str_shuffle('Original');
     $anime->set("source", $animeSource);
-    $this->assertEquals($anime->get("source"), $animeSource);
+    $this->assertEquals($animeSource, $anime->get("source"));
   }
 
   public function testGenres() {
     $anime = new Anime();
     $animeGenres = array(str_shuffle('Comedy'), str_shuffle('Drama'));
     $anime->set("genres", $animeGenres);
-    $this->assertEquals($anime->get("genres"), $animeGenres);
+    $this->assertEquals($animeGenres, $anime->get("genres"));
   }
 
   public function testDuration() {
@@ -168,36 +168,36 @@ Fast forward two and a half years and two of those members, Aoi Miyamori and Ema
     $animeDuration_PerEpisode = rand(1, 60);
     $anime->set("duration_total", $animeDuration_Total);
     $anime->set("duration_per_episode", $animeDuration_PerEpisode);
-    $this->assertEquals($anime->get("duration_total"), $animeDuration_Total);
-    $this->assertEquals($anime->get("duration_per_episode"), $animeDuration_PerEpisode);
+    $this->assertEquals($animeDuration_Total, $anime->get("duration_total"));
+    $this->assertEquals($animeDuration_PerEpisode, $anime->get("duration_per_episode"));
   }
 
   public function testRating() {
     $anime = new Anime();
     $animeRating = str_shuffle("PG-13 - Teens 13 or older");
     $anime->set("rating", $animeRating);
-    $this->assertEquals($anime->get("rating"), $animeRating);
+    $this->assertEquals($animeRating, $anime->get("rating"));
   }
 
   public function testMembers_Scored() {
     $anime = new Anime();
     $animeMembers_Scored = rand();
     $anime->set("members_scored", $animeMembers_Scored);
-    $this->assertEquals($anime->get("members_scored"), $animeMembers_Scored);
+    $this->assertEquals($animeMembers_Scored, $anime->get("members_scored"));
   }
 
   public function testMembers_InList() {
     $anime = new Anime();
     $animeMembers_InList = rand();
     $anime->set("members_inlist", $animeMembers_InList);
-    $this->assertEquals($anime->get("members_inlist"), $animeMembers_InList);
+    $this->assertEquals($animeMembers_InList, $anime->get("members_inlist"));
   }
 
   public function testMembers_Favorited() {
     $anime = new Anime();
     $animeMembers_Favorited = rand();
     $anime->set("members_favorited", $animeMembers_Favorited);
-    $this->assertEquals($anime->get("members_favorited"), $animeMembers_Favorited);
+    $this->assertEquals($animeMembers_Favorited, $anime->get("members_favorited"));
   }
 
   public function testExternal_Links() {
@@ -213,14 +213,14 @@ Fast forward two and a half years and two of those members, Aoi Miyamori and Ema
       )
     );
     $anime->set("external_links", $animeExternal_Links);
-    $this->assertEquals($anime->get("external_links"), $animeExternal_Links);
+    $this->assertEquals($animeExternal_Links, $anime->get("external_links"));
   }
 
   public function testBackground() {
     $anime = new Anime();
     $animeBackground = str_shuffle("<i>Shirobako</i> won the Animation Kobe Television Award in 2015.");
     $anime->set("background", $animeBackground);
-    $this->assertEquals($anime->get("background"), $animeBackground);
+    $this->assertEquals($animeBackground, $anime->get("background"));
   }
 
   public function testRelated() {
@@ -247,7 +247,7 @@ Fast forward two and a half years and two of those members, Aoi Miyamori and Ema
       )
     );
     $anime->set("related", $animeRelated);
-    $this->assertEquals($anime->get("related"), $animeRelated);
+    $this->assertEquals($animeRelated, $anime->get("related"));
   }
 
   public function testTheme_Songs() {
@@ -266,13 +266,13 @@ Fast forward two and a half years and two of those members, Aoi Miyamori and Ema
       )
     );
     $anime->set("theme_songs", $animeTheme_Songs);
-    $this->assertEquals($anime->get("theme_songs"), $animeTheme_Songs);
+    $this->assertEquals($animeTheme_Songs, $anime->get("theme_songs"));
   }
 
   public function testAsArray() {
     $anime = new Anime();
     $array = $anime->asArray();
-    $this->assertEquals($array["id"], null);
+    $this->assertEquals(null, $array["id"]);
   }
 }
 ?>
