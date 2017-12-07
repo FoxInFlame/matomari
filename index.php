26eccda771c2ccf80d1da938b0f4adcee686965e
FoxInFlame <burningfoxinflame@gmail.com>
2017-12-07 14:52:29 +0900

    Add Google verification meta

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
     <meta name="theme-color" content="#2e51a2"/> <!-- Because fancy colours are nice on Android -->
     <!--GOOGLE-->
-    <meta name="google-site-verification" content="inVPN1xlnM4Obo0dZ1mpkQD3h5T9_1VvLue303ehCbA" />
+    <meta name="google-site-verification" content="Tu1uvU-GrbGcqSljN0HtOIHb5SVu_LpwQsR5eUGeJRg" />
     <!--END GOOGLE-->
     <title>Introduction - matomari API</title>
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
