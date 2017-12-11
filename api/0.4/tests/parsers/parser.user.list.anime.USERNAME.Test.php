6f7ead491a8d588c01bdcc66dd702922904b6735
FoxInFlame <burningfoxinflame@gmail.com>
2017-12-12 00:22:45 +0900

    Unit test the parser for user/list/anime/:username

---
+++ api/0.4/tests/parsers/parser.user.list.anime.USERNAME.Test.php
@@ -0,0 +1,45 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+require_once(dirname(__FILE__) . "/../../src/parsers/parser.user.list.anime.USERNAME.php");
+
+class UserListAnimeParserTest extends TestCase {
+
+  private $anime; 
+
+  public function testParse() {
+    $xmlcontent = file_get_contents(dirname(__FILE__) . "/../mal_pages/mal_page.malappinfo.Drutol.xml");
+    $jsoncontent = file_get_contents(dirname(__FILE__) . "/../mal_pages/mal_page.listload.Drutol.json");
+
+    return UserListAnimeParser::parse($xmlcontent, $jsoncontent);
+  }
+
+  /**
+  * @depends testParse
+  */
+  public function testType($anime) {
+    $this->assertInternalType('array', $anime);
+    $this->assertInternalType('array', $anime[0]);
+    $this->assertInternalType('array', $anime[1]);
+  }
+
+  /**
+  * @depends testParse
+  */
+  public function testStats($anime) {
+    $this->assertEquals(259, $anime[0]["total"]);
+  }
+
+  /**
+  * @depends testParse
+  */
+  public function testAnime($anime) {
+    $this->assertEquals(1, $anime[1][0]["id"]);
+    $this->assertEquals(array(
+      "synonyms" => array("Cowboy Bebop")), $anime[1][0]["other_titles"]);
+    $this->assertEquals("2016-10-20", $anime[1][0]["watch_dates"]["from"]);
+    $this->assertEquals("2016-10-31T18:46:23+00:00", $anime[1][0]["last_updated"]);
+  }
+  
+}
+?>
