50bec50543e6112dd4c987f463e88260f933d705
FoxInFlame <burningfoxinflame@gmail.com>
2017-12-11 18:01:58 +0900

    More switch/cases!

--- api/0.4/src/models/model.php
+++ api/0.4/src/models/model.php
@@ -11,6 +11,25 @@ class Model {
 
   public function set($data, $value) {
     switch($data) {
+      case "air_status":
+        switch($value) {
+          case $value === 1:
+          case $value === "airing":
+            $value = 1;
+            break;
+          case $value === 2:
+          case $value === "finished airing":
+            $value = 2;
+            break;
+          case $value === 3:
+          case $value === "not yet aired":
+            $value = 3;
+            break;
+          default:
+            throw new ModelValueNotValid("The provided value is invalid.");
+        }
+        $this->info["air_status"] = $value;
+        break;
       case "air_date_from":
         $this->info["air_dates"]["from"] = $value;
         break;
@@ -60,15 +79,15 @@ class Model {
         switch($value) {
           case $value === 0:
           case $value === "low":
-            $value = 0;
+            $value = "low";
             break;
           case $value === 1:
           case $value === "medium":
-            $value = 1;
+            $value = "medium";
             break;
           case $value === 2:
           case $value === "high":
-            $value = 2;
+            $value = "high";
             break;
           default:
             throw new ModelValueNotValid("The provided value is invalid.");
@@ -79,14 +98,17 @@ class Model {
         switch($value) {
           case $value === 0:
           case $value === null:
+          case $value === "":
             $value = null;
             break;
           case $value === 1:
           case $value === "hard_drive":
+          case $value === "HD":
             $value = 1;
             break;
           case $value === 2:
           case $value === "dvd_cd":
+          case $value === "DVD":
             $value = 2;
             break;
           case $value === 3:
@@ -95,22 +117,27 @@ class Model {
             break;
           case $value === 4:
           case $value === "retail_dvd":
+          case $value === "RDVD":
             $value = 4;
             break;
           case $value === 5:
           case $value === "vhs":
+          case $value === "VHS":
             $value = 5;
             break;
           case $value === 6:
           case $value === "external_hd":
+          case $value === "EHD":
             $value = 6;
             break;
           case $value === 7:
           case $value === "nas":
+          case $value === "NAS":
             $value = 7;
             break;
           case $value === 8:
           case $value === "bluray":
+          case $value === "Blue-ray":
             $value = 8;
             break;
           default:
