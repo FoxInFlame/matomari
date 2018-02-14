64ed4ba57fb59e298b4d59faad71367bff6d9f77
FoxInFlame <burningfoxinflame@gmail.com>
2018-02-15 00:35:19 +0900

    Fix bugs

--- src/components/Exceptions.php
+++ src/components/Exceptions.php
@@ -1,4 +1,13 @@
 <?php
+
+/**
+ * A part of the matomari API.
+ * 
+ * @since 0.5
+ * @author FoxInFlame <burningfoxinflame@gmail.com>
+ * @version 0.5
+ */
+
 class MatomariError extends Exception {}
   
 class ModelKeyDoesNotExist extends Exception {}
--- src/components/Time.php
+++ src/components/Time.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * A part of the matomari API.
  * 
