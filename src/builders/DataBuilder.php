857312b65060c56fd0827cef20c1d1121a945389
FoxInFlame <burningfoxinflame@gmail.com>
2018-02-28 19:35:48 +0900

    Add caching to search + smaller functions for filtering queries

---
+++ src/builders/DataBuilder.php
@@ -0,0 +1,85 @@
+<?php
+
+/**
+ * A part of the matomari API.
+ * 
+ * @since 0.5
+ * @author FoxInFlame <burningfoxinflame@gmail.com>
+ * @version 0.5
+ */
+
+namespace Matomari\Builders;
+
+/**
+ * Return array of data for the request.
+ * 
+ * @since 0.5
+ * @author FoxInFlame <burningfoxinflame@gmail.com>
+ */
+class DataBuilder
+{
+
+  /**
+   * Array returned from cache or request.
+   * @var Array
+   */
+  private $array;
+
+  /**
+   * Build Array.
+   * 
+   * @param String $cache_key The key for retrieving/setting cache.
+   * @param Function $responsearray The response associative array.
+   * @since 0.5 
+   */
+  public function build($cache_key, $fallback) {
+    
+    // Initialise the driver: FileSystem
+    // We should set a cache path because the default one makes the keys too long.
+    $driver = new \Stash\Driver\FileSystem(array('path' => '/tmp/cache/'));
+
+    // Initialise the Stash Pool using the driver.
+    $pool = new \Stash\Pool($driver);
+
+    // Try to get an item from the pool using $cache_key.
+    $item = $pool->getItem($cache_key);
+    $data = $item->get();
+
+    // Set ValidationMethod to OLD so it uses the old values when a new one is updating.
+    $item->setInvalidationMethod(\Stash\Invalidation::OLD);
+
+    if($item->isMiss()) {
+
+      // Lock the item so new requests don't fall into the same path.
+      $item->lock();
+
+      // The response should contain [Data Array, Time Limit in Seconds]
+      $fallback_response = $fallback();
+
+      // Set the cache data to the one retrieved in the fallback.
+      $data = $fallback_response[0];
+      $item->set($fallback_response[0]);
+
+      // Set the cache timeout to the one retrieved in the fallback.
+      $item->expiresAfter($fallback_response[1]);
+
+      // Save the newly made cache item to the pool.
+      $pool->save($item);
+
+    }
+
+    $this->array = $data;
+
+  }
+
+  /**
+   * Retrieve the built Array.
+   * 
+   * @return Array
+   * @since 0.5
+   */
+  public function getArray() {
+    return $this->array;
+  }
+  
+}
--- src/collections/AnimeSearchCollection.php
+++ src/collections/AnimeSearchCollection.php
@@ -13,6 +13,7 @@ namespace Matomari\Collections;
 use GuzzleHttp\Client;
 use Matomari\Exceptions\MatomariError;
 use Matomari\Collections\Collection;
+use Matomari\Builders\DataBuilder;
 use Matomari\Parsers\AnimeSearchParser;
 
 /**
@@ -96,7 +97,8 @@ class AnimeSearchCollection extends Collection
       'psychological' => '40',
       'thriller' => '41',
       'seinen' => '42',
-      'josei' => '43'
+      'josei' => '43',
+      'default' => ''
     ]
   ];
 
@@ -108,7 +110,7 @@ class AnimeSearchCollection extends Collection
    * @param String $query The query to search for
    * @param String $page The raw unparsed page number 
    * @param String $sort The raw unparsed sorting specifier
-   * @param Array $filter The filters, parsed into an associative array
+   * @param Array $filters The filters, parsed into an associative array
    *  $filter = [
    *    'type' => (string) Show results matching the media type
    *    'score' => (string) Show results with scores above the score
@@ -121,131 +123,386 @@ class AnimeSearchCollection extends Collection
    *    ]
    *    'letter' => (string) Show results that start with the alphabet letter
    *    'genres' => (array) 
-   *    'genre_type' => (string) Either include or exclude the previous array of genres
+   *    'exclude_genres' => (string) Either include or exclude the previous array of genres
    *  ]
    * @since 0.5
    */
   public function __construct($query, $page, $sort, $filters) {
 
-    $filter_param = '';
-    foreach($filters as $filter_name => $filter_value) {
-      if($filter_value === '') {
-        continue;
+    // Create an array of URL parameters for sending the MAL request.
+    $parameters = $this->prepare_parameters($query, $page, $sort, $filters);
+
+    // Generate the cache key using the parameters sent into this function.
+    $cache_key = $this->generate_cache_key($query, $page, $sort, $filters);
+
+    // Initiate a DataBuilder.
+    $data_builder = new DataBuilder();
+
+    // Retrieve the data from the cache (using $cache_key), or from MAL using the fallback.
+    $data_builder->build($cache_key, function() use ($parameters) {
+
+      $guzzle_client = new Client();
+      $response = $guzzle_client->request('GET', 'https://myanimelist.net/anime.php', [
+        'http_errors' => false,
+        'query' => $parameters
+      ]);
+      
+
+      if($response->getStatusCode() === 429) {
+        throw new MatomariError('Too many frequent requests. Please wait and retry.', 429);
+      } else if($response->getStatusCode() === 404) {
+        throw new MatomariError('The page could not be found.', 404);
+      } else if($response->getStatusCode() !== 200) {
+        throw new MatomariError('There was an unknown error with MAL. Contact FoxInFlame.', 500);
       }
 
-      switch(strtolower($filter_name)) {
-        case 'type':
-          // type: TV, OVA, Movie, Special, ONA, Music.
-          $filter_param .= '&type=';
-          if(isset($this->mapping['type'][$filter_value])) {
-            $filter_param .= $this->mapping['type'][strtolower($filter_value)];
-          } else {
-            $filter_param .= $this->mapping['type']['default'];
-          }
-          break;
-        case 'score':
-          if(is_numeric($filter_value) && (int)$filter_value >= 1 && $filter_value <= 10) {
-            $filter_param .= '&score=' . $filter_value;
-          } else {
-            $filter_param .= '&score=0';
-          }
-          break;
-        case 'air_status':
-          $filter_param .= '&status=';
-          if(isset($this->mapping['air_status'][strtolower($filter_value)])) {
-            $filter_param .= $this->mapping['air_status'][strtolower($filter_value)];
-          } else {
-            $filter_param .= $this->mapping['air_status']['default'];
-          }
-          break;
-        case 'producer':
-          // TODO: Do something here...
-          break;
-        case 'classification':
-          $filter_param .= '&r=';
-          if(isset($this->mapping['classification'][strtolower($filter_value)])) {
-            $filter_param .= $this->mapping['classification'][strtolower($filter_value)];
-          } else {
-            $filter_param .= $this->mapping['classification']['default'];
-          }
-          break;
-        case 'air_dates':
-          if($filter_value['from']) {
-            if(strlen($filter_value) != 8) {
-              $filter_param .= '&sm=0&sd=0&sy=0';
-              break;
-            }
-            $sy = $sm = $sd = '0';
-            if(is_numeric(substr($filter_value, 0, 4))) {
-              $sy = (int)substr($filter_value, 0, 4);
-            }
-            if(is_numeric(substr($filter_value, 4, 2))) {
-              $sm = (int)substr($filter_value, 4, 2);
-            }
-            if(is_numeric(substr($filter_value, 6, 2))) {
-              $sd = (int)substr($filter_value, 6, 2);
-            }
-            $filter_param .= '&sy=' . $sy . '&sm=' . $sm . '&sd=' . $sd;
-          } else if($filter_value['to']) {
-            if(strlen($filter_value) != 8) {
-              $filter_param .= '&em=0&ed=0&ey=0';
-              break;
-            }
-            $ey = $em = $ed = '0';
-            if(is_numeric(substr($filter_value, 0, 4))) {
-              $ey = (int)substr($filter_value, 0, 4);
-            }
-            if(is_numeric(substr($filter_value, 4, 2))) {
-              $em = (int)substr($filter_value, 4, 2);
-            }
-            if(is_numeric(substr($filter_value, 6, 2))) {
-              $ed = (int)substr($filter_value, 6, 2);
-            }
-            $filter_param .= '&ey=' . $ey . '&em=' . $em . '&ed=' . $ed;
-          }
-          break;
-        case 'letter':
-          if(strlen($filter_value) != 1) {
-            break;
-          }
-          if(!preg_match('/^[a-zA-Z]$/', $filter_value)) {
-            break;
-          }
-          $filter_param .= '&letter=' . $filter_value;
-          break;
-        case 'genres':
-          foreach($filter_value as $filter_value_genre) {
-            if(isset($this->mapping['genres'][strtolower($filter_value_genre)])) {
-              $filter_param .= '&genre[]=' . $this->mapping['genres'][strtolower($filter_value_genre)];
-            }
-          }
-          break;
-        case 'exclude_genres':
-          if($filter_value) {
-            $filter_param .= '&gx=1';
-          }
-          break;
-      }
-    }
+      // Return the Data Arary and the Cache Timeout in seconds.
+      return [
+        AnimeSearchParser::parse($response->getBody()),
+        3600
+      ];
+    });
+  
+    $this->array = $data_builder->getArray();
 
+  }
+
+  /**
+   * Prepares all the URL parameters that are to be sent in the MAL search request.
+   * 
+   * @param String $query
+   * @param String $page
+   * @param String $sort 
+   * @param Array $filters
+   * @return Array
+   */
+  private function prepare_parameters($query, $page, $sort, $filters) {
+
+    $query_parameter = $this->prepare_query_parameter($query);
+
+    $page_parameter = $this->prepare_page_parameter($page);
+
+    $sort_parameter = $this->prepare_sort_parameter($sort);
+
+    $filter_parameters = $this->prepare_filter_parameters($filters);
+
+    return [
+      'c'=> ['a', 'b', 'c', 'd', 'e', 'f', 'g']
+    ] + $query_parameter + $page_parameter + $sort_parameter + $filter_parameters;
+
+    // ?q=' . urlencode($query) . $filter_param . '&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g' . $page_param, ['http_errors' => false]); // TODO: add sort
+  }
+
+  /**
+   * Prepare the search query segment of the Request url.
+   * 
+   * @param String $query
+   * @return Array
+   */
+  private function prepare_query_parameter($query) {
+    
+    // MAL does not accept search strings less than 3 bytes, but it somehow accepts spaces.
     if(strlen($query) < 3) {
       $query = $query . str_repeat(' ', 3 - strlen($query));
     }
 
-    $page_param = '&show=' . ($page - 1) * 50;
+    return [
+      'q' => urlencode($query)
+    ];
 
-    $guzzle_client = new Client();
-    $response = $guzzle_client->request('GET', 'https://myanimelist.net/anime.php?q=' . urlencode($query) . $filter_param . '&c[]=a&c[]=b&c[]=c&c[]=d&c[]=e&c[]=f&c[]=g' . $page_param, ['http_errors' => false]);
+  }
 
-    if($response->getStatusCode() === 429) {
-      throw new MatomariError('Too many frequent requests. Please wait and retry.', 429);
-    } else if($response->getStatusCode() === 404) {
-      throw new MatomariError('The page could not be found.', 404);
-    } else if($response->getStatusCode() !== 200) {
-      throw new MatomariError('There was an unknown error with MAL. Contact FoxInFlame.', 500);
+  /**
+   * Prepare the page segment of the Request url.
+   * 
+   * @param String $page
+   * @return Array
+   */
+  private function prepare_page_parameter($page) {
+    
+    // The show parameter is a multiple of 50, and it is how much data is has shown before.
+    // So, 0 for the first page, 50 for the second page.
+    $show_offset = ((int)$page - 1) * 50;
+
+    return [
+      'show' => $show_offset
+    ];
+
+  }
+
+  /**
+   * Prepare the sort segment of the Request url.
+   * TODO: Complete this.
+   * 
+   * @param String $sort
+   * @return Array
+   */
+  private function prepare_sort_parameter($sort) {
+    return [];
+  }
+
+  /**
+   * Prepare the filter segment of the Request url.
+   * 
+   * @param Array $filters
+   * @return Array
+   */
+  private function prepare_filter_parameters($filters) {
+    
+    $filter_parameters = [];
+    foreach($filters as $filter_name => $filter_value) {
+
+      if($filter_value === '') continue;
+
+      if($filter_name === 'type') {
+        $filter_parameter = $this->prepare_simple_filter_parameter('type', 'type', $filter_value);
+      } else if($filter_name === 'score') {
+        $filter_parameter = $this->prepare_score_filter_parameter('score', $filter_value);
+      } else if($filter_name === 'air_status') {
+        $filter_parameter = $this->prepare_simple_filter_parameter('status', 'air_status', $filter_value);
+      } else if($filter_name === 'producer') {
+        throw new MatomariError('Not yet implemented.', 501);
+      } else if($filter_name === 'classification') {
+        $filter_parameter = $this->prepare_simple_filter_parameter('r', 'classification', $filter_value);
+      } else if($filter_name === 'air_dates') {
+        $filter_parameter = $this->prepare_dates_filter_parameter($filter_value);
+      } else if($filter_name === 'letter') {
+        $filter_parameter = $this->prepare_letter_filter_parameter($filter_value);
+      } else if($filter_name === 'genres') {
+        $filter_parameter === $this->prepare_genre_filter_parameter($filter_value);
+      } else if($filter_name ==='exclude_genres') {
+        if($filter_value) $filter_param = ['gx' => '1'];
+      } else continue;
+
+      $filter_parameters = $filter_parameters + $filter_parameter;
+      
+    }
+    return $filter_parameters;
+  }
+
+  /**
+   * Prepare a simple filter parameter that reads from the mapping.
+   * 
+   * @param String $url_key The key for the URL segment to be generated
+   * @param String $mapping_key The key to find the key=>value mappings for the URL segment
+   * @param String $value The value of the filter that is to be searched in the mapping.
+   * @return Array
+   */
+  private function prepare_simple_filter_parameter($url_key, $mapping_key, $value) {
+
+    // If the URL mapping for the segment key exists, use it, if not use the 'default' key.
+    if(isset($this->mapping[$mapping_key][strtolower($value)])) {
+      $url_value = $this->mapping[$mapping_key][strtolower($value)];
+    } else {
+      $url_value = $this->mapping[$mapping_key]['default'];
     }
 
-    $this->array = AnimeSearchParser::parse($response->getBody());
+    return [
+      $url_key => $url_value
+    ];
+
+  }
+
+  /**
+   * Prepare the score filter parameter that converts values into integers from 1-10.
+   * 
+   * @param String $url_key The key for the URL segment to be generated
+   * @param String $value The input score
+   * @return Array
+   */
+  private function prepare_score_filter_parameter($url_key, $value) {
+
+    // If the value is numeric, and is between 1 and 10
+    if(is_numeric($value) && (int)$value >= 1 && (int)$value <= 10) {
+      $url_value = (string)(int)$value; // Just... in case it's a floating number or something.
+    } else {
+      $url_value = '0';
+    }
+    return [
+      $url_key = $url_value
+    ];
+
+  }
+
+  /**
+   * Prepare the date filter parameters. Converts dates (full and partial) into MAL parameters.
+   * 
+   * @param Array $value Array containg 'from' and 'to' keys.
+   * @return Array
+   */
+  private function prepare_dates_filter_parameter($value) {
+
+    // This is the array that will be returned at the end of this function
+    $date_parameters = [];
+
+    if($value['from']) {
+      if(preg_match('/(?:^|\s|$)\d{4}-\d{2}-\d{2}(?:^|\s|$)/', $value['from'], $matches)) {
+        // YYYY-MM-DD
+        $date_parameters['sy'] = explode('-', $matches[0])[0];
+        $date_parameters['sm'] = explode('-', $matches[0])[1];
+        $date_parameters['sd'] = explode('-', $matches[0])[2];
+      } else if(preg_match('/(?:^|\s|$)\d{4}-\d{2}--(?:^|\s|$)/', $value['from'], $matches)) {
+        // YYYY-MM--
+        $date_parameters['sy'] = explode('-', $matches[0])[0];
+        $date_parameters['sm'] = explode('-', $matches[0])[1];
+        $date_parameters['sd'] = '0';
+      } else if(preg_match('/(?:^|\s|$)\d{4}----(?:^|\s|$)/', $value['from'], $matches)) {
+        // YYYY-MM--
+        $date_parameters['sy'] = explode('-', $matches[0])[0];
+        $date_parameters['sm'] = '0';
+        $date_parameters['sd'] = '0';
+      } else if(preg_match('/(?:^|\s|$)\d{4}---\d{2}(?:^|\s|$)/', $value['from'], $matches)) {
+        // YYYY---DD
+        $date_parameters['sy'] = explode('-', $matches[0])[0];
+        $date_parameters['sm'] = '0';
+        $date_parameters['sd'] = explode('---', $matches[0])[1];
+      } else if(preg_match('/(?:^|\s|$)--\d{2}-\d{2}(?:^|\s|$)/', $value['from'], $matches)) {
+        // --MM-DD
+        $date_parameters['sy'] = '0';
+        $date_parameters['sm'] = explode('-', $matches[0])[2];
+        $date_parameters['sd'] = explode('-', $matches[0])[3];
+      } else if(preg_match('/(?:^|\s|$)----\d{2}(?:^|\s|$)/', $value['from'], $matches)) {
+        // ----DD
+        $date_parameters['sy'] = '0';
+        $date_parameters['sm'] = '0';
+        $date_parameters['sd'] = explode('----', $matches[0])[1];
+      } else if(preg_match('/(?:^|\s|$)--\d{2}--(?:^|\s|$)/', $value['from'], $matches)) {
+        // --MM--
+        $date_parameters['sy'] = '0';
+        $date_parameters['sm'] = explode('--', $matches[0])[1];
+        $date_parameters['sd'] = '0';
+      } else {
+        // -----
+        // Invalid Dates
+        $date_parameters['sy'] = '0';
+        $date_parameters['sm'] = '0';
+        $date_parameters['sd'] = '0';
+      }
+    }
+
+    if($value['to']) {
+      if(preg_match('/(?:^|\s|$)\d{4}-\d{2}-\d{2}(?:^|\s|$)/', $value['to'], $matches)) {
+        // YYYY-MM-DD
+        $date_parameters['ey'] = explode('-', $matches[0])[0];
+        $date_parameters['em'] = explode('-', $matches[0])[1];
+        $date_parameters['ed'] = explode('-', $matches[0])[2];
+      } else if(preg_match('/(?:^|\s|$)\d{4}-\d{2}--(?:^|\s|$)/', $value['to'], $matches)) {
+        // YYYY-MM--
+        $date_parameters['ey'] = explode('-', $matches[0])[0];
+        $date_parameters['em'] = explode('-', $matches[0])[1];
+        $date_parameters['ed'] = '0';
+      } else if(preg_match('/(?:^|\s|$)\d{4}----(?:^|\s|$)/', $value['to'], $matches)) {
+        // YYYY-MM--
+        $date_parameters['ey'] = explode('-', $matches[0])[0];
+        $date_parameters['em'] = '0';
+        $date_parameters['ed'] = '0';
+      } else if(preg_match('/(?:^|\s|$)\d{4}---\d{2}(?:^|\s|$)/', $value['to'], $matches)) {
+        // YYYY---DD
+        $date_parameters['ey'] = explode('-', $matches[0])[0];
+        $date_parameters['em'] = '0';
+        $date_parameters['ed'] = explode('---', $matches[0])[1];
+      } else if(preg_match('/(?:^|\s|$)--\d{2}-\d{2}(?:^|\s|$)/', $value['to'], $matches)) {
+        // --MM-DD
+        $date_parameters['ey'] = '0';
+        $date_parameters['em'] = explode('-', $matches[0])[2];
+        $date_parameters['ed'] = explode('-', $matches[0])[3];
+      } else if(preg_match('/(?:^|\s|$)----\d{2}(?:^|\s|$)/', $value['to'], $matches)) {
+        // ----DD
+        $date_parameters['ey'] = '0';
+        $date_parameters['em'] = '0';
+        $date_parameters['ed'] = explode('----', $matches[0])[1];
+      } else if(preg_match('/(?:^|\s|$)--\d{2}--(?:^|\s|$)/', $value['to'], $matches)) {
+        // --MM--
+        $date_parameters['ey'] = '0';
+        $date_parameters['em'] = explode('--', $matches[0])[1];
+        $date_parameters['ed'] = '0';
+      } else {
+        // -----
+        // Invalid Dates
+        $date_parameters['ey'] = '0';
+        $date_parameters['em'] = '0';
+        $date_parameters['ed'] = '0';
+      }
+    }
+
+    return $date_parameters;
+
+  }
+
+  /**
+   * Prepare the letter filter parameter. 
+   * 
+   * @param String $value Unparsed alphabetical letter to filter out the results.
+   * @return Array
+   */
+  private function prepare_letter_filter_parameter($value) {
+
+    $url_value = [];
+    if(strlen($value) === 1) {
+      if(preg_match('/^[a-zA-Z]$/', $value)) {
+        $url_value = strtolower($value); // MAL does accept capital letters but just in case
+      }
+    }
+
+    return [
+      'letter' => $url_value
+    ];
+
+  }
+
+  /**
+   * Prepare the genre filters from an array of genres. Use prepare_simple_filter_parameter().
+   * 
+   * @param Array $value Genres to include or exclude
+   * @return Array
+   */
+  private function prepare_genre_filter_parameter($value) {
+    
+    $genre_parameters = [];
+
+    // Loop through each genre and create a simple filter.
+    foreach($value as $genre) {
+      $genre_parameters = array_merge_recursive(
+        $genre_parameters,
+        $this->prepare_simple_filter_parameter('genre', 'genres', $genre)
+      );
+    }
+
+    return $genre_parameters;
+
+  }
+
+  /**
+   * Generate a directory-like cache key that Stash can read.
+   * 
+   * @param String $query
+   * @param String $page
+   * @param String $sort
+   * @param Array $filters
+   * @return String
+   */
+  private function generate_cache_key($query, $page, $sort, $filters) {
+
+    // Hash and cut down the query to a 12 letter string so it doesn't trigger a Too Long Key error.
+    $query = substr(base_convert(md5($query), 16, 32), 0, 12);
+
+    if(!empty($filters)) {
+      
+      // Hash and cut down the filters as well, but only if it's not empty.
+      // Use json_encode() instead of serialize() because it is faster and produces a smaller string.
+      $filters = substr(base_convert(md5(json_encode($filters)), 16, 32), 0, 12);
+
+      // Add a slash so that it can continue with page.
+      $filters = $filters . '/';
+
+    } else {
+
+      $filters = '';
+
+    }
+
+    return '/anime/search' . $query . '/' . $filters . (string)(int)$page;
+
   }
 
 }
