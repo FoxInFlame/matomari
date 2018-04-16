26d0f50e4288006d1801460ebd631103f9ed00b0
FoxInFlame <burningfoxinflame@gmail.com>
2018-04-16 17:58:21 +0900

    Add ReDoc documentation

---
+++ docs/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>matomari Docs</title>
+    <!-- needed for mobile devices -->
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+  </head>
+  <body>
+    <redoc spec-url="../api/0.5/"></redoc>
+    <script src="https://cdn.jsdelivr.net/npm/redoc@next/bundles/redoc.standalone.js"></script>
+  </body>
+</html>
