05a57681149e7a4879a9c745a78f20e5458c4543
FoxInFlame <burningfoxinflame@gmail.com>
2018-12-16 13:33:38 +0900

    SEO Optimization for docs page

--- docs/index.php
+++ docs/index.php
@@ -1,9 +1,23 @@
 <!DOCTYPE html>
 <html>
   <head>
-    <title>matomari Docs</title>
+    <title>matomari API - Unofficial MyAnimeList API - Documentation</title>
+    <meta name="description" content="The matomari API is an unofficial MyAnimeList API designed to offer features no other MAL API does. Written in PHP by FoxInFlame.">
+    <meta name="keywords" content="matomari,MyAnimeList,PHP,API,Unofficial,Fast,Anime,Manga">
+    <meta property="og:title" content="matomari API - Unofficial MAL API - Documentation">
+    <meta property="og:description" content="The matomari API is an unofficial MyAnimeList API designed to offer features no other MAL API does!">
+    <meta property="og:url" content="https://www.matomari.tk/docs/">
+    <meta name="twitter:title" content="matomari API - Unofficial MAL API - Documentation">
+    <meta name="twitter:description" content="The matomari API is an unofficial MyAnimeList API designed to offer features no other MAL API does!">
+    <meta name="twitter:site" content="@FoxInFlame">
     <!-- needed for mobile devices -->
     <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="canonical" href="https://www.matomari.tk/docs/">
+    <style>
+      html, body {
+        margin: 0
+      }
+    </style>
   </head>
   <body>
     <redoc spec-url="../api/0.5/"></redoc>
